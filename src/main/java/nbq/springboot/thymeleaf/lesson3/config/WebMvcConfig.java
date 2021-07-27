6f1143a7764ced4c8fbf57ef1c5de2783e89a9f5
QuynhNB <NBQProgramming@gmail.com>
2021-07-28 06:19:36 +0700

    Create login form and áºhandling 403 áºerror

--- src/main/java/nbq/springboot/thymeleaf/lesson3/config/WebMvcConfig.java
+++ src/main/java/nbq/springboot/thymeleaf/lesson3/config/WebMvcConfig.java
@@ -9,5 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/403").setViewName("/error/403");
+        registry.addViewController("/login").setViewName("/security/login");
     }
 }
--- src/main/java/nbq/springboot/thymeleaf/lesson3/config/WebSecurityConfig.java
+++ src/main/java/nbq/springboot/thymeleaf/lesson3/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package nbq.springboot.thymeleaf.lesson3.config;
 
+import nbq.springboot.thymeleaf.lesson3.handlerException.CustomAccessDeniedHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,14 +11,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                .withUser("admin")
+               //pass: QuynhVan
                .password("$2a$10$MQZ5cHvlpNDeMrJp/3xPWepJ0gUz0Lj0RpjZnjWZ1vtY./D/zWld.")
                .roles("ADMIN")
            .and()
                .withUser("nbquynh")
+               //pass: QuynhVan
                .password("$2a$10$MQZ5cHvlpNDeMrJp/3xPWepJ0gUz0Lj0RpjZnjWZ1vtY./D/zWld.")
                .roles("USER");
     }
@@ -24,14 +31,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","/products").permitAll()
+                // Các đường dẫn của các file tĩnh phải được thêm vào permit ALl , để trang dùng tài nguyên tĩnh có thể chạy bình thường mà không phải login
+                .antMatchers("/","/index","/products","/webjars/**").permitAll()
                 .antMatchers("/products/new").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/products/edit/*", "/products/delete/*" ).hasRole("ADMIN")
                 .anyRequest().authenticated()
+                /** HTTP Basic Authentication
                 .and()
                 .httpBasic()
                 .and()
                 .exceptionHandling().accessDeniedPage("/403")
-                ;
+                ;*/
+                .and()
+            .formLogin()
+                .loginPage("/login")
+
+                //Default URL sử lý khi login : /login , trong action attribute của <form>  Ta có thể custom URL bằng: loginProcessingUrl("customURL")
+                // Phải điều chỉnh giá trị tương ứng với thuộc tính action của <form> trong login page.
+                /*.loginProcessingUrl("doLogin")*/
+
+                // giá trị mặc định của trường name của form login  là : username, password. Ta có thể tùy chỉnh điều này bằng 2 phương thức dưới đây
+                /*.usernameParameter("custom_username")
+                .passwordParameter("custom_password")*/
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll()
+                .and()
+            .exceptionHandling()
+                // Khi thực hiện thêm việc lưu log lại các lỗi ,hoặc làm thêm một vài tác vụ nào đó khi lỗi xảy ra, ta có thể thực thi  custom AccessDeniedHandler
+                .accessDeniedHandler(customAccessDeniedHandler)
+            ;
     }
 }
